bee97753e9cf564c99c99b7fb66e94f6de9180f5
Cheeseday <aleksandrkirilushkin@gmail.com>
2023-11-04 06:55:34 +0100

    feat: implement pagination

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import './App.scss';
 import Loader from './components/loader/Loader';
 import SearchBar from './components/searchBar/SearchBar';
 import CharactersList from './components/charactersList/CharactersList';
+import Pagination from './components/pagination/Pagination';
+import { flushSync } from 'react-dom';
+
+type ResponseType = {
+  results: string[];
+  count: number;
+};
 
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [inputValue, setInputValue] = useState('');
   const [charactersList, setCharactersList] = useState(['']);
+  const [totalCount, setTotalCount] = useState(82);
+  const [siblingCount] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize] = useState(10);
   const inputValueHandler = (): void => {
     if (event) setInputValue((event.target as HTMLInputElement)?.value);
   };
@@ -18,19 +29,25 @@ const App: React.FC = () => {
   const searchHandler = (): void => {
     setIsLoading(true);
     setCharactersList([]);
-
-    let search = '';
-    const lastSearch = getSearchKey();
-    if (lastSearch) {
-      search = `?search=${lastSearch}`;
-    }
-    if (inputValue) {
-      search = `?search=${inputValue}`;
+    const currentPageNumber = currentPage > 0 ? currentPage : 1;
+    console.log(currentPage);
+    let queryString = `?page=${currentPageNumber}`;
+    const querySearchParam = getSearchKey();
+    if (querySearchParam && !inputValue) {
+      queryString += `&search=${querySearchParam}`;
+    } else if (inputValue) {
+      queryString += `&search=${inputValue}`;
       localStorage.setItem('searchKey', JSON.stringify(inputValue));
     }
-    getCharacters(search, setIsLoading).then((value) =>
-      setCharactersList(value)
-    );
+    getResponse(queryString, setIsLoading).then((response) => {
+      setCharactersList(response.results);
+      setTotalCount(response.count);
+    });
+  };
+  const pageChangeHandler = (page: number): void => {
+    flushSync(() => setCurrentPage(page));
+    // setCurrentPage(page); //force update
+    searchHandler();
   };
   const getSearchKey = (): string => {
     const value = localStorage.getItem('searchKey');
@@ -44,29 +61,42 @@ const App: React.FC = () => {
           inputValueHandler={inputValueHandler}
         />
         <CharactersList charactersList={charactersList} />
+        <Pagination
+          className="paginationBar"
+          totalCount={totalCount}
+          currentPage={currentPage}
+          siblingCount={siblingCount}
+          pageSize={pageSize}
+          onPageChange={(page) => pageChangeHandler(page)}
+        />
       </main>
       <div>{isLoading ? <Loader /> : <div></div>}</div>
     </Fragment>
   );
 };
 
-async function getCharacters(
+async function getResponse(
   value: string,
   loadingSetter: React.Dispatch<React.SetStateAction<boolean>>
-): Promise<Array<string>> {
+): Promise<ResponseType> {
   const url = `https://swapi.dev/api/people/${value}`;
-  let charactersList: Array<string> = [];
-  charactersList = await fetch(`${url}`)
+  // const url = `https://swapi.dev/api/people/${value}&page=5`;
+  let answer: ResponseType = {
+    results: [],
+    count: 0,
+  };
+  answer = await fetch(`${url}`)
     .then((response) => response.json())
     .then((response) => {
-      return response.results;
+      console.log(response);
+      return response;
     })
     .catch((e: Error) => {
       console.log(e.message);
-      return [];
+      return {};
     });
   loadingSetter(false);
-  return charactersList;
+  return answer;
 }
 
 export default App;
